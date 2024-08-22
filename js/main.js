a073e46fa5abf4c84f9bee16dd6fdcc43fb8bcea
felixs-alt <114471609+felixs-alt@users.noreply.github.com>
2024-08-22 13:17:16 +0200

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// Hello there!
+//
+// If you want to add my games to your site, please reach out at my email: echo-the-coder@tuta.io, or discord: 3kh0_#6969
+
+
+
+// ====================================
+// SCRIPT INJECTION
+// ====================================
+const gogascript27 = document.createElement("script");
+gogascript27.setAttribute("defer", "");
+gogascript27.setAttribute("src", "https://static.cloudflareinsights.com/beacon.min.js");
+gogascript27.setAttribute("data-cf-beacon", '{"token": "72d0b1a399144c9ab4f72e6da597a9a7"}');
+document.head.append(gogascript27);
+                    
