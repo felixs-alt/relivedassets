156a5e904155e2dd9a31285bccba15866910a8cf
felixs-alt <114471609+felixs-alt@users.noreply.github.com>
2024-10-23 13:35:07 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,11 +2,15 @@
 // SCRIPT INJECTION
 // ====================================
 const addScript = async src => new Promise((resolve, reject) => {
-    const el = document.createElement('script');
-    el.src = src;
-    el.addEventListener('load', resolve);
-    el.addEventListener('error', reject);
-    document.body.append(el);
+  const el = document.createElement('script');
+  el.src = src;
+  el.addEventListener('load', resolve);
+  el.addEventListener('error', reject);
+  console.log(document.body)
+  document.body.appendChild(el)
 });
-await addScript('https://cdn.socket.io/4.8.0/socket.io.min.js');
-await addScript('/js/socket.js');
+
+document.addEventListener("DOMContentLoaded", async function() {
+  await addScript('https://cdn.socket.io/4.8.0/socket.io.min.js');
+  await addScript('/js/socket.js');
+});
